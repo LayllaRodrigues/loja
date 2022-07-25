cd24d9e05148e19877415da28e1fbc7411de5261
Laylla Rodrigues <laylla.oliveira.rodrigues@hotmail.com>
2022-07-25 20:40:36 -0300

    adicionando funcionalidade remove produtos

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  delete "produtos/:id", to: "produtos#destroy", as: :produto
   post "produtos", to: "produtos#create"
   get "produtos/new", to: "produtos#new"
   get "produtos", to: "produtos#index"
