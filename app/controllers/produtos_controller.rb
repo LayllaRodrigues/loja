42714516f8805c6aa7aae4f4078ce69e7ff08ca9
Laylla Rodrigues <laylla.oliveira.rodrigues@hotmail.com>
2022-07-25 17:38:30 -0300

    redirecionando para menu apos cadastro de novo produto

--- app/controllers/produtos_controller.rb
+++ app/controllers/produtos_controller.rb
@@ -3,4 +3,10 @@ class ProdutosController < ApplicationController
         @produtos = Produto.order(nome: :desc).limit 3
         @produto_com_desconto = Produto.order(:preco).limit 1
     end
+
+    def create 
+        produto = params.require(:produto).permit(:nome, :descricao, :preco, :quantidade)
+        Produto.create produto
+        redirect_to root_path
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  post "produtos", to: "produtos#create"
   get "produtos/new", to: "produtos#new"
   get "produtos", to: "produtos#index"
   root to: "produtos#index"
