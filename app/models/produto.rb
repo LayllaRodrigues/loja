feffcac794d9bbc214b250d6a21446ed6d500551
Laylla Rodrigues <laylla.oliveira.rodrigues@hotmail.com>
2022-07-26 13:49:07 -0300

    validando que campos preco, nome, descricao e qauntidade nao estao em branco

--- app/models/produto.rb
+++ app/models/produto.rb
@@ -1,4 +1,6 @@
 class Produto < ApplicationRecord
     validates :preco, presence: true
-     
+    validates :nome, length: {minimum: 4}
+    validates :descricao, presence: true
+    validates :quantidade, presence: true
 end
