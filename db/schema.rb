13682a11131b211ddbc9004fc526859859b9cdd9
Laylla Rodrigues <laylla.oliveira.rodrigues@hotmail.com>
2022-07-26 14:24:17 -0300

    criando departamentos e ajustando ux de navegacao

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_24_224344) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_26_170539) do
+  create_table "departamentos", force: :cascade do |t|
+    t.string "nome"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "produtos", force: :cascade do |t|
     t.string "nome"
     t.text "descricao"
