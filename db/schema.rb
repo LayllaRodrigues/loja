eaab8a7413a965a52e1728e279ee5266610a4c49
Laylla Rodrigues <laylla.oliveira.rodrigues@hotmail.com>
2022-07-26 14:38:50 -0300

    criando relacionamento para tabelas

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_26_170539) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_26_172705) do
   create_table "departamentos", force: :cascade do |t|
     t.string "nome"
     t.datetime "created_at", null: false
@@ -24,6 +24,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_26_170539) do
     t.integer "quantidade"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "departamento_id"
   end
 
 end
