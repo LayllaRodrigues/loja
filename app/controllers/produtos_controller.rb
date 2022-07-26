8db72b2ebe1eb9ccfc5116f731ffdff6488092a3
Laylla Rodrigues <laylla.oliveira.rodrigues@hotmail.com>
2022-07-26 19:16:13 -0300

    refatorando com partials e locals

--- app/controllers/produtos_controller.rb
+++ app/controllers/produtos_controller.rb
@@ -1,53 +1,62 @@
 class ProdutosController < ApplicationController
+
+    before_action :set_produto, only: [:edit, :update, :destroy]
+
     def index
-        @produtos = Produto.order(nome: :desc).limit 3
+        @produtos = Produto.order(nome: :asc).limit 6
         @produto_com_desconto = Produto.order(:preco).limit 1
     end
-
+    
     def new
         @produto = Produto.new
         @departamentos = Departamento.all
     end
-
-    def create 
-        valores = params.require(:produto).permit(:nome, :descricao, :preco, :quantidade, :departamento_id)
-        @produto = Produto.new valores
-        if @produto.save 
-            flash[:notice] = "Produto salvo com sucesso!"
-            redirect_to root_url
-        else
-            render :new
-        end
+    
+    def edit
+        renderiza :edit
     end
 
     def update
-        id = params[:id]
-        @produto = Produto.find(id)
-        valores = params.require(:produto).permit(:nome, :descricao, :preco, :quantidade, :departamento_id)
-        if @produto.update valores
+        if @produto.update produto_params
             flash[:notice] = "Produto atualizado com sucesso!"
             redirect_to root_url
         else
-            render :new
-            @departamentos = Departamento.all
+            renderiza :edit
+        end
+    end
+    
+    def create
+        @produto = Produto.new produto_params
+        if @produto.save
+            flash[:notice] = "Produto salvo com sucesso!"
+            redirect_to root_url
+        else
+            renderiza :new
         end
     end
 
-    def edit
-        id = params[:id]
-        @produto = Produto.find(id)
-        @departamentos = Departamento.all 
-        render :new
-    end
-
-    def destroy 
-        id = params[:id]
-        Produto.destroy id 
+    def destroy
+        @produto.destroy
         redirect_to root_url
     end
-
+    
     def busca
         @nome = params[:nome]
         @produtos = Produto.where "nome like ?", "%#{@nome}%"
+    end
+    
+    private
+
+    def produto_params
+        params.require(:produto).permit(:nome, :descricao, :preco, :quantidade, :departamento_id)
+    end
+
+    def set_produto
+        @produto = Produto.find(params[:id])
+    end
+    
+    def renderiza(view)
+        @departamentos = Departamento.all 
+        render view
     end
 end
